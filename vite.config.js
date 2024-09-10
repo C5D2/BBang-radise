b5d2b1cb8180a46f89762d8cc212fd4c353ff9c3
C5D2 <yoakec5d2@naver.com>
2024-09-10 12:29:40 +0900

    🔧 react-snap 설정 관련 코드 추가

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+  },
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
