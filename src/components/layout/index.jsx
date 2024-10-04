bd1901b9e19a7b5eee91727f4bcbe4b720d70131
C5D2 <yoakec5d2@naver.com>
2024-10-04 21:03:26 +0900

    ✨ 메뉴 이동 시 스크롤 상단으로 이동 기능 구현

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,12 @@
 import Footer from '@components/layout/Footer';
 import Header from '@components/layout/Header';
+import ScrollToTop from '@components/ui/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
